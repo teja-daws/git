de9c87d5aa1a2cf7e2cd41412322d944ac026723
teja-daws <tejadevops94@gmail.com>
2024-11-19 16:19:05 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,7 +4,7 @@ echo "Please enter your username::"
 
 read -s USERNAME #takes input into USERNAME variable
 
-echo "Username entered is: $USERNMAE"
+echo "Username entered is: $USERNAME"
 
 echo "Please enter your password::"
 
