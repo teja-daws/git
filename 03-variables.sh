2a9059c65dcb5f8350ba6dacf51aa88fb80f0d20
teja-daws <tejadevops94@gmail.com>
2024-11-19 15:56:10 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#DRY --> Don't repeat yourself
+#Centralised place --> If you change at one place, it will update everywhere, reduced human errors & no accidental changes
+
+PERSON1=Raheem
+PERSON2=Suresh
+
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hello $PERSON1, I am fine, How you are doing"
+echo "$PERSON1: I am doing good $PERSON2, what's going on"
+echo "$PERSON2: I started learning shell scripting $PERSON1"
