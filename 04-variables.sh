7b8b0193f7fbdd317fd39badad99a6b0b5a91cf7
teja-daws <tejadevops94@gmail.com>
2024-11-19 16:08:29 +0530

    variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,6 +6,6 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1: Hi $PERSON2, How are you?"
-echo "$PERSON2: Hello $PERSON1, I am fine, How you are doing"
+echo "$PERSON2: Hello $PERSON1, I am fine, How you doing"
 echo "$PERSON1: I am doing good $PERSON2, what's going on"
 echo "$PERSON2: I started learning shell scripting $PERSON1"
