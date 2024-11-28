65adcf65efe4740daf781fb136d8f58622d6288c
teja-daws <tejadevops94@gmail.com>
2024-11-28 08:25:02 +0530

    adding shell scripts

--- 11-installation.sh
+++ 11-installation.sh
@@ -32,7 +32,7 @@ if [ $? -ne 0 ]
 then
     echo "mysql is not installed... going to install it"
     dnf install mysql -y 
-    if [ $?-ne 0 ]
+    if [ $? -ne 0 ]
     then
         echo "mysql installation is failure... please check"
         exit 1
