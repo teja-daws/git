afc1c183ac209cfb98ab2d4865195dbbbedce19b
teja-daws <tejadevops94@gmail.com>
2024-11-29 13:41:57 +0530

    adding shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -17,7 +17,7 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "$R Please run this script with root priveleges $N"
+        echo "$R Please run this script with root priveleges $N" &>>$LOG_FILE
         exit 1
     fi
 }
@@ -25,10 +25,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is...$G SUCCESS $N"
+        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
