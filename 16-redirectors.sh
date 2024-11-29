e50e3f13b87c2b7501bcd20cebf2251013b04912
teja-daws <tejadevops94@gmail.com>
2024-11-29 15:06:42 +0530

    adding shell

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -3,7 +3,7 @@
 #/var/log/
 
 LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 |cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
