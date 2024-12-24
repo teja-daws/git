9b12ec65813612e0ef4272507d351011e36d0eac
teja-daws <tejadevops94@gmail.com>
2024-12-24 08:46:34 +0530

    modifying redirectors script

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -18,7 +18,7 @@ N="\e[0m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo "$R Please run this script with root priveleges $N" &>>$LOG_FILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a &>>$LOG_FILE
         exit 1
     fi
 }
@@ -26,19 +26,19 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
+        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE | tee -a &>>$LOG_FILE
     fi
 }
 
 USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2..."
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2..." | tee -a &>>$LOG_FILE
     exit 1
 }
 
-echo "Script started excuting at $(date)"
+echo "Script started excuting at $(date)" | tee -a &>>$LOG_FILE
 
 CHECK_ROOT
 
@@ -52,10 +52,10 @@ do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it..." &>>$LOG_FILE
+        echo "$package is not installed, going to install it..." | tee -a &>>$LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package" &>>$LOG_FILE
     else
-        echo -e "$package is already $Y installed... nothing to do $N" &>>$LOG_FILE
+        echo -e "$package is already $Y installed... nothing to do $N" | tee -a &>>$LOG_FILE
     fi
 done
