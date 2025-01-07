d90d7b2fd959cf0dec080da5777b729b2cf40b51
teja-daws <tejadevops94@gmail.com>
2025-01-07 13:19:01 +0530

    shell

--- 19-backup.sh
+++ 19-backup.sh
@@ -3,6 +3,7 @@
 SOURCE_DIR=$1
 DEST_DIR=$2
 DAYS=${3:-14} #if #3 is empty, default is 14 days
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 
 USAGE(){
     echo -e "$R USAGE:: $N sh 19-backup.sh <source> <destination> <days(optional)>"
@@ -31,6 +32,23 @@ echo "Files: $FILES"
 if [ ! -z $FILES ] #true if files is empty, ! makes expressiom fase
 then
     echo "Files are found"
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name ".log" -mtime +14 | zip "$ZIP_FILE" -@
+
+    #check if zip file is successfully created or not
+    if [ -f $ZIP_FILE ]
+    then
+        echo "Sucessfully zipped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS, internal filed seperator, empty it will ignore while space, -r is for not to ignore special characters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Zipping the files is failed"
+        exit 1
+    fi
 else
     echo "No files older than $DAYS"
 fi
