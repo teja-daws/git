241361d88fbe9911b7a9c867701fdc4223d3c813
teja-daws <tejadevops94@gmail.com>
2025-01-09 13:20:08 +0530

    ansible

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 set -e #setting the automatic exit, if we get error; set -ex for debug
+
+failure(){
+    echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND"' ERR #ERR is the error signal
 echo "Hello world success"
 echooooo "Hello world failure"
 echo "Hello world after failure"
